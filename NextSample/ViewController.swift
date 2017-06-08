fdf661e555cfa4e2e4381ac2e92599e4a329e2b9
ESS Mac Pro <ESSMacPro@essmacbookpro.local>
2017-06-08 09:20:53 -0400

    Commit All Changes

---
+++ NextSample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NextSample
+//
+//  Created by ESS Mac Pro on 6/8/17.
+//  Copyright © 2017 Pyar.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
